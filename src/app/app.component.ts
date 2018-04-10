5ebf3273ab78e7e4471ebc6fba0f7ddc45759eb5
DimaAdmin <ryba-mech@yandex.ru>
2018-04-10 17:23:30 +0300

    04- create a new Article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,11 @@ export class AppComponent {
       new Article('Angular Homepage', 'http://angular.io', 1),
     ];
   }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
 }
