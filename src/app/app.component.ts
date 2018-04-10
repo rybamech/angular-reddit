579299674ac1af82e1d1e5f26feb3e6bf979f41b
DimaAdmin <ryba-mech@yandex.ru>
2018-04-10 17:15:49 +0300

    04- all articles rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import {Article} from './article/article.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    return false;
+  articles: Article[]; // <-- component property
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
   }
 }
